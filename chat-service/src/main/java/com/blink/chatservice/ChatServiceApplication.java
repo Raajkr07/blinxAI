0f2516e988544da29cc3a365f22b528a5c17afee
RAJ KUMAR <rk8210032@gmail.com>
2026-01-16 14:31:08 +0530

    add bean for MCP tool

--- chat-service/src/main/java/com/blink/chatservice/ChatServiceApplication.java
+++ chat-service/src/main/java/com/blink/chatservice/ChatServiceApplication.java
@@ -1,11 +1,11 @@
 package com.blink.chatservice;
 
 import com.blink.chatservice.chat.service.ChatServiceImpl;
-import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,6 +15,7 @@ import java.util.List;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@EnableCaching
 public class ChatServiceApplication {
 
     public static void main(String[] args) {
